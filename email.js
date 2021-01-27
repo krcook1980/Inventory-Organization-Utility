3ad1ca9f3ef550283d2fe95bd00ff599313820f2
Jason Jorgensen <jasoncjorgensen@gmail.com>
2021-01-26 19:46:24 -0700

    adding the working email api

--- email.js
+++ email.js
@@ -1,1 +1,18 @@
-alert ("working");
+
+$(".btn").click(function(){
+    // alert("working");
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "peanutbutterkellys2021@gmail.com",
+        Password : "peanutbutterkellys",
+        To : 'jasoncjorgensen@gmail.com',
+        From : "inventory@hafabricators.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+      
+    );
+
+
+})
