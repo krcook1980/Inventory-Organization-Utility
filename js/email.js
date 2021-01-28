9dd69605103cae666317b651ff69854cb5c5047e
Jason Jorgensen <jasoncjorgensen@gmail.com>
2021-01-27 18:39:24 -0700

    Adding local storage function

--- js/email.js
+++ js/email.js
@@ -1,16 +1,32 @@
 
 
+var material = localStorage.getItem("materialChoice");
+var width = localStorage.getItem("width");
+var plateThickness = localStorage.getItem("plateThickness");
+var quantity = localStorage.getItem("quantity");
+var action = localStorage.getItem("Action");
+
+
+
+
+
+
 $(".emailButton").click(function(){
     // alert("working");
     Email.send({
     SecureToken : "8bdfc5e6-1f38-4818-a994-73f350628a8e",
     To : 'jasoncjorgensen@gmail.com',
     From : "inventory@hafabricators.com",
-    Subject : "This is the subject",
+    Subject : action +" of "+material,
     Body : "And this is the body"
 }).then(function(message){
     console.log(message)
-    $(".emailNotification").append("<p>"+message+"</p>")
+    if (message=="OK"){
+        $(".emailNotification").append("<p>"+"Email sent successfully!"+"</p>");
+    }
+    else {
+        $(".emailNotification").append("<p>"+"EMAIL ERROR"+"</p>")
+    }
 }
 //   message => console.log(message)
 //   $(".emailNotification").append("<p>"+message+"</p>")
