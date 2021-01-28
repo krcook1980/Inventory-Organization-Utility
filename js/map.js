d3903daa404f0f6700cc97334ef699ae59bfd284
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-01-27 21:00:21 -0700

    finalize logic

--- js/map.js
+++ js/map.js
@@ -157,14 +157,169 @@ else if(type == "Tubing" && width == "4"){
       });
   $("#location").text("Found in A3/B3");
 }
+else if(type == "Tubing" && width == "6"){
+  const marker1 = new google.maps.Marker({
+    position: { lat: 41.78064, lng: -111.84182 },
+    map: map,
+    title: "B1",
+    });
+   
+  const marker2 = new google.maps.Marker({
+      position: { lat: 41.78044, lng: -111.84182 },
+      map: map,
+      title: "B2",
+      });
+  $("#location").text("Found in B1/B2");
+}
 
+else if(type == "Angle" && width == "3"){
+  const marker1 = new google.maps.Marker({
+    position: { lat: 41.78064, lng: -111.84187, },
+    map: map,
+    title: "C1",
+    });
+   
+  const marker2 = new google.maps.Marker({
+      position: { lat: 41.78044, lng: -111.84187, },
+      map: map,
+      title: "C2",
+      });
+  $("#location").text("Found in C1/C2");
+}
+else if(type == "Angle" && width == "4"){
+  const marker1 = new google.maps.Marker({
+    position: { lat: 41.78024, lng: -111.84187, },
+    map: map,
+    title: "C3",
+    });
+   
+  const marker2 = new google.maps.Marker({
+      position: { lat: 41.78024, lng: -111.84203, },
+      map: map,
+      title: "D3",
+      });
+  $("#location").text("Found in C3/D3");
+}
+else if(type == "Angle" && width == "4"){
+  const marker1 = new google.maps.Marker({
+    position: { lat: 41.78064, lng: -111.84203, },
+    map: map,
+    title: "D1",
+    });
+   
+  const marker2 = new google.maps.Marker({
+      position: { lat: 41.78044, lng: -111.84203, },
+      map: map,
+      title: "D2",
+      });
+  $("#location").text("Found in D1/D2");
+}
 
-//make a marker
-/*const marker = new google.maps.Marker({
-  position: { lat: 41.78074, lng: -111.84170 },
-  map: map,
-  title: "A1",
-  });
-//}
-*/
+else if(type=="Plate" && width=="5"){
+  if(thick==".25"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78064, lng: -111.84208, },
+      map: map,
+      title: "E1",
+      });
+
+  }
+  if(thick==".5"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78044, lng: -111.84208, },
+      map: map,
+      title: "E2",
+      });
+  }
+  if(thick==".75"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78024, lng: -111.84208, },
+      map: map,
+      title: "E3",
+      });
+  }
+  $("#location").text("Found in " + title);
+}
+
+else if(type=="Plate" && width=="6"){
+  if(thick==".25"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78064, lng: -111.84223, },
+      map: map,
+      title: "F1",
+      });
+
+  }
+  if(thick==".5"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78044, lng: -111.84223, },
+      map: map,
+      title: "F2",
+      });
+  }
+  if(thick==".75"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78024, lng: -111.84223, },
+      map: map,
+      title: "F3",
+      });
+  }
+  $("#location").text("Found in " + title);
+}
+
+else if(type=="Plate" && width=="8"){
+  if(thick==".25"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78064, lng: -111.84228, },
+      map: map,
+      title: "G1",
+      });
+
+  }
+  if(thick==".5"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78044, lng: -111.84228, },
+      map: map,
+      title: "G2",
+      });
+  }
+  if(thick==".75"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78024, lng: -111.84228, },
+      map: map,
+      title: "G3",
+      });
+  }
+  $("#location").text("Found in " + title);
+}
+
+else if(type=="Plate" && width=="12"){
+  if(thick==".25"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78064, lng: -111.84243, },
+      map: map,
+      title: "H1",
+      });
+
+  }
+  if(thick==".5"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78044, lng: -111.84243, },
+      map: map,
+      title: "H2",
+      });
+  }
+  if(thick==".75"){
+    const marker1 = new google.maps.Marker({
+      position: { lat: 41.78024, lng: -111.84243, },
+      map: map,
+      title: "H3",
+      });
+  }
+  $("#location").text("Found in " + title);
+}
+else{
+  $("#location").text("No location found");
+}
+
 }
